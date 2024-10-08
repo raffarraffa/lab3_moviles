349b5f7f156ebf2b36f58cc2203c1055c909b0e0
Rafael Lopez <50971821+raffarraffa@users.noreply.github.com>
2024-10-08 10:40:44 -0300

    entrega tp1

---
+++ tp1_share_preference/app/src/main/java/com/rafalopez/tp1_sharedpref/ui/profile/ProfileActivityViewModel.java
@@ -0,0 +1,68 @@
+package com.rafalopez.tp1_sharedpref.ui.profile;
+
+import android.app.Application;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
+import com.rafalopez.tp1_sharedpref.model.Usuario;
+import com.rafalopez.tp1_sharedpref.request.ApiCliente;
+import com.rafalopez.tp1_sharedpref.ui.login.LoginActivity;
+
+public class ProfileActivityViewModel extends AndroidViewModel {
+    //private Intent intent;
+    private Context context;
+    public MutableLiveData<Usuario> mUsuario =new MutableLiveData<>();
+    public  MutableLiveData<Integer> mRegistroError;
+    public ProfileActivityViewModel(@NonNull Application application){
+        super(application);
+        context=application.getApplicationContext();
+    }
+    public LiveData<Usuario> getMusuario(){
+        if(mUsuario==null){
+            mUsuario= new MutableLiveData<>();
+        }
+        return mUsuario;
+    }
+
+    public LiveData<Integer> getMRegistroError(){
+        if(mRegistroError==null){
+            mRegistroError= new MutableLiveData<>();
+        }
+        return mRegistroError;
+    }
+
+    public void setRegistro(Usuario u){
+        if(!u.isValid()){
+            mRegistroError.setValue(0);
+            Toast.makeText(context, "Error de registro \n verifique datos",Toast.LENGTH_LONG).show();
+            return;
+        }
+        ApiCliente.guardar(context,u);
+        Toast.makeText(context, "Datos guardados \n Identifiquese",
+                Toast.LENGTH_LONG).show();
+        mUsuario.setValue(u);
+        Intent intent=new Intent(context, LoginActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(intent);
+    }
+    public void getRegistro(boolean login){
+        if(!login) return ;
+        Usuario u = ApiCliente.leerDatos(context);
+        if(u==null) return;
+        mUsuario.setValue(u);
+    }
+    public int verifyDni(String dni ){
+        if(dni.isEmpty()){
+            return 0;
+        }
+        return  Integer.parseInt(dni);
+    }
+}
